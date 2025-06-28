8952630a49d586e73738ab08add087b6fe6cd058
Stillbit <215258983+StillbitTech@users.noreply.github.com>
2025-06-28 23:18:04 +0500

    Delete scripts/helpers.js

--- scripts/helpers.js
+++
@@ -1,7 +0,0 @@
-
-export function timeAgo(timestamp) {
-  const diff = Date.now() - timestamp;
-  if (diff < 60000) return 'Just now';
-  if (diff < 3600000) return Math.floor(diff / 60000) + ' min ago';
-  return Math.floor(diff / 3600000) + ' hrs ago';
-}
